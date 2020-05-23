67c807c15aedea4506632c9882f89b8fd5eb79ba
AnonCracker1 <65563381+AnonCracker1@users.noreply.github.com>
2020-05-23 19:59:57 +0300

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,19 @@
+"""Check if userbot alive."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("**`Baithe baithe hue bore…..Karna hai Kuch kaam (Shuru kro yeh userbot leke prabhu ka naam) ψ(｀∇´)ψ`**\n\n"
+                     "**Telethon version:- 6.9.0**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**Python: 3.7.3**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
+                     "**Bot Made By:- @anon_cracker\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**"
+                     "**Database Status: Databases functioning normally!**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\nAlways with you, my peru master!\n`"
+                     f"`My peru owner`: {DEFAULTUSER}\n"
+                     "[Deploy this userbot Now](https://github.com/AnonCracker1/AnonCracker/)")
